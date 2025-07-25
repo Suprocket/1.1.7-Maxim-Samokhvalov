3626ad6a18ed12aaa4a8eaba4b6abd2a5f1a4b3d
Suprocket <mpsamoxvalov@gmail.com>
2025-07-26 01:28:43 +0300

    initial project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createElement } from 'react';
+import ReactDOM from 'react-dom/client';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const element = createElement('h3', { className: 'title' }, 'У меня в штанах две пушки. Один - огнестрел. Второй - для подружки.  © Джейсон Cтетхэм');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(element);
